27babf1d8206c3232b161009cf0dd96344ee3a69
SihikamYGY <hikamgggeming@gmail.com>
2026-07-14 10:37:38 +0700

    push

---
+++ dart-comment.dart
@@ -0,0 +1,25 @@
+// single comment
+// Digunakan menonaktifkan kode baris atau
+// menandakan sebuah catatan
+
+/* Multiline Comment
+* Biasanya digunakan untuk menjelaskan sebuah
+* function/logika
+*/
+
+/// Documentation Comment [Main]
+/// Aturan dari Documentation Comment:
+/// 1. Diawali dengan ///
+/// 2. Untuk memanggil function menggunakan []
+/// [age]
+
+void main(List<String> args) {
+  // mencetak Hello Guys!
+  print('Hello Guys!');
+
+  print('object');
+}
+
+int age(){
+  return 17;
+}
