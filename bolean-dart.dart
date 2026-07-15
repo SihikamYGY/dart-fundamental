370ea98cc1b51259a4de74ad02e193ec28aa1e81
SihikamYGY <hikamgggeming@gmail.com>
2026-07-15 11:12:40 +0700

    Implement main function with error handling and arithmetic operations

--- bolean-dart.dart
+++ bolean-dart.dart
@@ -1,3 +1,21 @@
 import 'dart:io';
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  var a = 7;
+  var b = 0;
+
+  try {
+    var hasil = a ~/ b; // Operator pembagian integer
+    print('Hasil: $hasil');
+  } catch (e) {
+    print('Terjadi kesalahan: $e');
+  }
+
+  try {
+    print(a + b);
+  } catch (e) {
+    print('Terjadi kesalahan: $e');
+  } finally {
+    print('Operasi selesai.');
+  }
+}
